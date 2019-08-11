2433ab526a440f0e057d3826d2ba2fbd3a884858
S Sai Vineet <saivineet89@gmail.com>
2019-08-11 19:28:44 +0530

    Add resilience

--- main.py
+++ main.py
@@ -3,10 +3,13 @@
 # BITS Pilani, Goa campus
 # Goa cmapus best campus fuk u pilani pussies
 import requests
+import config
 
 import gi
 gi.require_version("Notify", "0.7")
 from gi.repository import Notify
+
+from enum import Enum
 
 
 Notify.init ("Auto login")
@@ -14,26 +17,46 @@ LOGIN_URL = "https://campnet.bits-goa.ac.in:8090/login.xml"
 USERNAME = config.USERNAME
 PASSWORD = config.PASSWORD
 
+
+TrialEnum = Enum("TrialResponse", "SUCCESS DATA_EXCEEDED ERROR")
+
+
 def show_notification(message):
     notif = Notify.Notification.new ("Auto login script",
                                      message,
                                      "dialog-information")
     notif.show ()
 
-data = {
-    "mode": 191,
-    "username": USERNAME,
-    "password": PASSWORD,
-    "a": 123456789,
-    "producttype": 0
-} 
 
-login = requests.post(LOGIN_URL, data = data)
-if ("LIVE" in login.text):
-    show_notification("You have been logged in successfully")
-elif ("data transfer" in login.text):
-    show_notification("Your data transfer has been exceeded")
-else:
-    show_notification("Something went wrong")
-    print (login.text)
+def try_login(username, password):
+    data = {
+        "mode": 191,
+        "username": username,
+        "password": password,
+        "a": 123456789,
+        "producttype": 0
+    } 
+
+    login = requests.post(LOGIN_URL, data = data)
+    if ("LIVE" in login.text):
+        return TrialEnum.SUCCESS
+    elif ("data transfer" in login.text):
+        return TrialEnum.DATA_EXCEEDED
+    else:
+        return TrialEnum.ERROR
+
+
+if __name__ == '__main__':
+    attempt = None
+    for i, (username, password) in enumerate(zip(USERNAME, PASSWORD)):
+        attempt = try_login(username, password)
+        if attempt is TrialEnum.SUCCESS:
+            # Finally post a notification
+            show_notification(f"Auto login succeeded on try {i+1}")
+            break
+    else:
+        if attempt is TrialEnum.DATA_EXCEEDED:
+            show_notification("Data exceeded in all IDs")
+        else:
+            show_notification("Something went wrong...")
 
